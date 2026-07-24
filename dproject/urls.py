6fdaf7a598223ba94f20751486f5640e8e5003e0
Shayra <shayra@gmail.com>
2026-07-24 16:20:30 +0530

    Move video to static folder for production support

--- dproject/urls.py
+++ dproject/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user.urls')),
 ]
-# Serve media files during development
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
